91c7ae46eeed226d9b261d51c77f1f33871a3073
Jennifer Nystrom <jennifer@getharvest.com>
2015-08-13 19:36:33 -0700

    hashes

---
+++ ruby_challenges/hashes.rb
@@ -0,0 +1,7 @@
+entry_01 = { name: "Jennifer", email: "jennifer@getharvest.com"}
+entry_02 = { name: "Karla", email: "karla@getharvest.com"}
+entry_03 = { name: "Arun", email: "arun@getharvest.com"}
+
+winner = entry_02[:name]
+
+puts "#{winner} is the contest winner!"
